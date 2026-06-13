c96da27a78120b06eed366cba6576da6b0ed262a
jvron <205451073+jvron@users.noreply.github.com>
2026-06-13 11:34:43 +0100

    fix(gl): properly create shaders before compilation

--- engine/src/renderer/opengl/gl_backend.cpp
+++ engine/src/renderer/opengl/gl_backend.cpp
@@ -1,4 +1,5 @@
 #include <glad/glad.h>
+
 #include "gl_backend.hpp"
 
 void GLBackend::clearBuffer(Color &clearColor) {
@@ -14,11 +15,12 @@ void GLBackend::uploadBuffer(GLuint buffer, size_t size , const void* data) {
     glNamedBufferStorage(buffer, size, data, GL_STATIC_DRAW);
 }
 
+// vertex array
 void GLBackend::createVertexArray(GLuint &vao) {
     glCreateVertexArrays(1, &vao);
 }
 
-void GLBackend::AttachVertexBuffer(GLuint vao, GLuint buffer, size_t size) {
+void GLBackend::attachVertexBuffer(GLuint vao, GLuint buffer, size_t size) {
     glVertexArrayVertexBuffer(vao, 0, buffer, 0, size);
 }
 
@@ -28,15 +30,17 @@ void GLBackend::setAttribute(GLuint vao, size_t offset) {
     glVertexArrayAttribBinding(vao, 0, 0);
 }
 
+//shaders
 GLuint GLBackend::compileVertShader(const char *shaderSource) {
-    GLuint vertexShader {};
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &shaderSource, NULL);
     glCompileShader(vertexShader);
     return vertexShader;
 }
 
 GLuint GLBackend::compileFragShader(const char *shaderSource) {
-    GLuint fragmentShader {};
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &shaderSource, NULL);
     glCompileShader(fragmentShader);
     return fragmentShader;
