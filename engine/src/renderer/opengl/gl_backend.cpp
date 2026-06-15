93ba4beffee92bf71eab85c916cd7e54e0713a85
jvron <205451073+jvron@users.noreply.github.com>
2026-06-15 16:38:38 +0100

    refactor(gl): improve VAO setup and buffer upload utilities

--- engine/src/renderer/opengl/gl_backend.cpp
+++ engine/src/renderer/opengl/gl_backend.cpp
@@ -1,6 +1,9 @@
+#include <cstdint>
 #include <glad/glad.h>
+#include <cstddef>
 
 #include "gl_backend.hpp"
+#include "engine/world.hpp"
 
 void GLBackend::clearBuffer(Color &clearColor) {
     glClearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
@@ -12,7 +15,7 @@ void GLBackend::createBuffer(GLuint &buffer) {
 }
 
 void GLBackend::uploadBuffer(GLuint buffer, size_t size , const void* data) {
-    glNamedBufferStorage(buffer, size, data, GL_STATIC_DRAW);
+    glNamedBufferStorage(buffer, size, data, 0); //immutable buffer
 }
 
 // vertex array
@@ -20,14 +23,22 @@ void GLBackend::createVertexArray(GLuint &vao) {
     glCreateVertexArrays(1, &vao);
 }
 
-void GLBackend::attachVertexBuffer(GLuint vao, GLuint buffer, size_t size) {
-    glVertexArrayVertexBuffer(vao, 0, buffer, 0, size);
+void GLBackend::attachVertexBuffer(GLuint vao, uint32_t bindingIndex, GLuint buffer, size_t offset, size_t stride) {
+    glVertexArrayVertexBuffer(vao, bindingIndex, buffer, offset, stride); // // attach VBO to binding slot n with given stride
 }
 
-void GLBackend::setAttribute(GLuint vao, size_t offset) {
-    glEnableVertexArrayAttrib(vao, 0);
-    glVertexArrayAttribFormat(vao, 0, 3, GL_FLOAT, GL_FALSE, offset);
-    glVertexArrayAttribBinding(vao, 0, 0);
+void GLBackend::attachElementBuffer(GLuint vao, GLuint buffer) {
+    glVertexArrayElementBuffer(vao, buffer);
+}
+
+void GLBackend::setAttribute(GLuint vao, uint32_t bindingIndex,const VertexAttribute &attribute) { 
+
+    glEnableVertexArrayAttrib(vao, attribute.location); // enable attribute layout location slot at specified location 
+
+    // set vertex attribute layout at the location
+    glVertexArrayAttribFormat(vao, attribute.location, attribute.componentCount, attribute.componentType, attribute.normalized, attribute.relativeOffset); 
+    // read from VBO at binding index n
+    glVertexArrayAttribBinding(vao, attribute.location, bindingIndex); 
 }
 
 //shaders
--- engine/src/renderer/opengl/gl_backend.hpp
+++ engine/src/renderer/opengl/gl_backend.hpp
@@ -1,9 +1,12 @@
 #pragma once
+//#include <glad/glad.h>
 #include <cstddef>
-#include <glad/glad.h>
-
 
 #include "ecs/components.hpp"
+#include "engine/world.hpp"
+#include "resources/resource_manager.hpp"
+
+struct GPUMesh;
 
 namespace GLBackend {
 
@@ -13,13 +16,14 @@ namespace GLBackend {
     void uploadBuffer(GLuint buffer, size_t size, const void* data); // upload data to the allocated GPU buffer
     
     void createVertexArray(GLuint &vao);
-    void AttachVertexBuffer(GLuint vao, GLuint buffer, size_t stride);
-    void setAttribute(GLuint vao, size_t offset);
+    void attachVertexBuffer(GLuint vao, uint32_t bindingIndex, GLuint buffer, size_t offset, size_t stride);
+    void attachElementBuffer(GLuint vao, GLuint buffer);
+    void setAttribute(GLuint vao, uint32_t bindingIndex, const VertexAttribute &attribute);
 
     GLuint compileVertShader(const char* shaderSource);
     GLuint compileFragShader(const char* shaderSource);
     GLuint createShaderProgram();
     void attachShader(GLuint &shaderProgram, GLuint &shader);
-    void deleteShader(GLuint &shader); 
+    void deleteShader(GLuint &shader);
     void linkProgram(GLuint &shaderProgram);
 }
