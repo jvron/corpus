92138b92402f470b559cec14932337f55c9e8e1d
jvron <205451073+jvron@users.noreply.github.com>
2026-06-21 21:09:28 +0100

    feat(systems): implement camera controller system with mouse look and FPS movement

---
+++ engine/include/engine/systems.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <glm/ext/matrix_float4x4.hpp>
+#include <glm/ext/vector_float3.hpp>
+
+#include "ecs/components.hpp"
+#include "engine/world.hpp"
+
+namespace CameraControlSystem {
+
+    static const glm::vec3 worldUp {0.0f, 1.0f, 0.0f};
+
+    void update(World& world);
+
+    void moveForward(Camera& camera, CameraController& ctrl, float dt);
+    void moveBackwards(Camera& camera, CameraController& ctrl, float dt);
+    void moveRight(Camera& camera, CameraController& ctrl, float dt);
+    void moveLeft(Camera& camera, CameraController& ctrl, float dt);
+}
---
+++ engine/src/systems/camera_controller.cpp
@@ -0,0 +1,56 @@
+#include <glm/trigonometric.hpp>
+
+#include "ecs/components.hpp"
+#include "engine/systems.hpp"
+#include "engine/world.hpp"
+#include "glm/ext/vector_float3.hpp"
+
+void CameraControlSystem::update(World& world) {
+    
+    CameraController& ctrl = world.registry.getComponent<CameraController>(world.activeCamera);
+    Camera& camera = world.registry.getComponent<Camera>(world.activeCamera);
+
+    InputState& inputState = world.engineState.inputState;
+
+    ctrl.yaw += inputState.mouseDeltaX * world.engineConfig.inputConfig.sensitivity * ctrl.sensitivityMultiplier;
+    ctrl.pitch += inputState.mouseDeltaY * world.engineConfig.inputConfig.sensitivity * ctrl.sensitivityMultiplier;
+
+    if (ctrl.pitch > 89.0f) {
+        ctrl.pitch = 89.0f;
+    }
+    if (ctrl.pitch < -89.0f) {
+        ctrl.pitch = -89.0f;
+    }
+
+    inputState.mouseDeltaX = 0.0f;
+    inputState.mouseDeltaY = 0.0f;
+
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(ctrl.yaw)) * cos(glm::radians(ctrl.pitch));
+    direction.y = sin(glm::radians(ctrl.pitch));
+    direction.z = sin(glm::radians(ctrl.yaw)) * cos(glm::radians(ctrl.pitch));
+    camera.front = glm::normalize(direction);
+
+    camera.right = glm::normalize(glm::cross(camera.front, worldUp));
+    camera.up = glm::normalize(glm::cross(camera.right, camera.front));
+}
+
+void CameraControlSystem::moveForward(Camera& camera, CameraController& ctrl , float dt) {
+
+    camera.position += camera.front * ctrl.speed * dt;
+}
+
+void CameraControlSystem::moveBackwards(Camera& camera, CameraController& ctrl, float dt) {
+
+    camera.position -= camera.front * ctrl.speed * dt;
+}
+
+void CameraControlSystem::moveRight(Camera& camera, CameraController& ctrl, float dt) {
+
+    camera.position += camera.right * ctrl.speed * dt;
+}
+
+void CameraControlSystem::moveLeft(Camera& camera,  CameraController& ctrl, float dt) {
+
+    camera.position -= camera.right * ctrl.speed * dt;
+}
