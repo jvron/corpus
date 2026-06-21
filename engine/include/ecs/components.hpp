7cfc30802a222ccd3edf5d7c5b9faf9c9741deff
jvron <205451073+jvron@users.noreply.github.com>
2026-06-21 19:38:39 +0100

    feat(ecs): add camera and camera controller components

--- engine/include/ecs/components.hpp
+++ engine/include/ecs/components.hpp
@@ -37,3 +37,22 @@ struct Material {
     ShaderHandle shaderHandle {};
     glm::vec4 baseColor {1.0f, 1.0f, 1.0f, 1.0f};
 };
+
+struct Camera {
+    float fov {45.0f};
+    float nearPlane {0.1f};
+    float farPlane {100.0f};
+
+    glm::vec3 position {0.0f, 0.0f, 0.0f};
+    glm::vec3 front {0.0f, 0.0f, -1.0f}; 
+    glm::vec3 up {0.0f, 1.0f, 0.0f};
+    glm::vec3 right {1.0f, 0.0f, 0.0f};
+};
+
+struct CameraController {
+    float speed {0.5f};
+    float yaw {};
+    float pitch {};
+
+    float sensitivityMultiplier {1.0f};
+};
