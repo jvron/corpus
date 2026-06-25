af034c157ae575e5fed4391ed7a1d59b65a9f011
jvron <205451073+jvron@users.noreply.github.com>
2026-06-25 15:13:04 +0100

    refactor(ecs): simplify View iterator and remove pointer indirection

--- engine/include/ecs/view.hpp
+++ engine/include/ecs/view.hpp
@@ -9,21 +9,38 @@
 #include "ecs/registry.hpp"
 #include "ecs/sparse_set.hpp"
 
+struct Entity;
+
 template <typename ... Component>
 class View {
 
 private:
     std::array<SparseSet*, sizeof...(Component)> poolPtrs;
-    SparseSet& leaderPool;
     Registry& registryRef;
+    SparseSet& leaderPool;
 
 public:
 
-    View(Registry &registry) : poolPtrs{registry.getPool<Component>()...}, leaderPool(getLeaderPool()), registryRef(registry) {};
+    View(Registry &registry) : poolPtrs{registry.getPool<Component>()...}, registryRef(registry), leaderPool(getLeaderPool()) {};
+
+    SparseSet& getLeaderPool() {
+
+        int leaderIndex = 0;
+        for (uint32_t i = 0; i < sizeof...(Component); i++) {
+            if (poolPtrs[i]->size() < poolPtrs[leaderIndex]->size()) {
+                leaderIndex = i;
+            }
+        }
+        return *poolPtrs[leaderIndex];
+    }
 
     class Iterator {
 
     private:
+        View& view;
+        SparseSet& pool;
+        uint32_t index {}; // current dense index
+
         void validateOrAdvance() {
             if (index >= pool.size()) {
                 return;
@@ -36,40 +53,32 @@ public:
 
                 for (uint32_t i = 0; i < sizeof...(Component); i++) {
 
-                    if (!view->poolPtrs[i]->hasEntity(entity)) {
+                    if (!view.poolPtrs[i]->hasEntity(entity)) {
                         isMatch = false;
                         break;
                     }
                 }
-                if (!isMatch) {
-                    index++;
-                }
-                else {
+
+                if (isMatch) {
                     return;
                 }
+                index++;
             }
         }
 
     public:
-        View* view;
-        SparseSet& pool;
-        uint32_t index {}; // current dense index
 
-        Iterator(View& v, SparseSet& p, uint32_t i) : view(&v), pool(p), index(i) {
+        Iterator(View& v, SparseSet& p, uint32_t i) : view(v), pool(p), index(i) {
             validateOrAdvance();
         }
 
         std::tuple<Entity, Component& ...> operator*() {
             Entity entity = pool.denseEntities[index];
-            std::tuple<Entity, Component& ...> tup(entity, view->registryRef.template getComponent<Component>(entity) ...);
-            return tup;
+            return {entity, view.registryRef.template getComponent<Component>(entity) ...} ;
         }
 
         bool operator!=(const Iterator& other) {
-            if (index != other.index) {
-                return true;
-            }
-            return false;
+            return index != other.index;
         }
 
         Iterator& operator++() {
@@ -87,14 +96,4 @@ public:
         return Iterator(*this, leaderPool, leaderPool.size());
     }
 
-    SparseSet& getLeaderPool() {
-
-        int leaderIndex = 0;
-        for (uint32_t i = 0; i < sizeof...(Component); i++) {
-            if (poolPtrs[i]->size() < poolPtrs[leaderIndex]->size()) {
-                leaderIndex = i;
-            }
-        }
-        return *poolPtrs[leaderIndex];
-    }
 };
