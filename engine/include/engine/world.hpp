af976a1a3fa4161a55bf99e696fdb009c9325c40
jvron <205451073+jvron@users.noreply.github.com>
2026-07-29 13:57:42 +0100

    feat(world): add scene manager

--- engine/include/engine/world.hpp
+++ engine/include/engine/world.hpp
@@ -3,6 +3,7 @@
 #include "ecs/components.hpp"
 #include "resources/resource_manager.hpp"
 #include "ecs/registry.hpp"
+#include "scene/scene_manager.hpp"
 
 constexpr int32_t keyCount = 348; 
 
@@ -78,6 +79,9 @@ struct World {
 
     Registry registry;
     ResourceManager resourceManager;
+    SceneManager sceneManager;
 
     Entity activeCamera;
+
+    World() : sceneManager(SceneManager(registry, resourceManager)) {}
 };
